043ea8ddc472dc234ddf0d4e858acae562c3c71d
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2024-07-03 15:58:00 -0700

    Format `test_commands.py` and update imports to appease pre-commit hooks

--- tests/functional/list/test_commands.py
+++ tests/functional/list/test_commands.py
@@ -3,11 +3,8 @@ import shutil
 import pytest
 
 from dbt.cli.types import Command
+from dbt.contracts.graph.nodes import BaseNode
 from dbt.tests.util import run_dbt
-from tests.functional.fixtures.happy_path_fixture import (  # noqa: F401
-    happy_path_project,
-    happy_path_project_files,
-)
 
 for i in Command:
     print(i.value)
@@ -33,11 +30,16 @@ commands = [
     "test",
     "retry",
 ]
-# flake8: noqa
-## Idea 1: define a mapping of commands-> actual commands to run, and a list to skip, make sure they add up to all commands
-# Run everything to make sure we are not missing any commands
-#
-# we can run everything?
+
+"""
+Idea 1: define a mapping of commands-> actual commands to run, and a list to skip, make sure they add up to all commands
+
+Run everything to make sure we are not missing any commands
+
+We can run everything?
+"""
+
+
 class TestRunCommands:
     @pytest.fixture(autouse=True)
     def drop_snapshots(self, happy_path_project, project_root: str) -> None:
@@ -57,12 +59,11 @@ class TestRunCommands:
             run_dbt([dbt_command])
 
 
-#### Idea 2:
-# compose all of the nodes being executed in the command before and test that all node type are covered?
-# Need to figure out what all nodes are
-
-
-from dbt.contracts.graph.nodes import BaseNode
+"""
+Idea 2:
+Compose all of the nodes being executed in the command before and test that all node type are covered?
+Need to figure out what all nodes are
+"""
 
 
 def find_end_classes(cls):
@@ -80,13 +81,15 @@ end_classes = find_end_classes(BaseNode)
 for cls in end_classes:
     print(cls)
 
-####Idea 3:
-# select resource type: make sure new nodes are selectable
-# check the resource type defined in params, make sure we can select all of them, and also that list plus the resource type that are not selectable defined in
-# a list here are echo to all of the resource types
+"""
+Idea 3:
+select resource type: make sure new nodes are selectable
+check the resource type defined in params, make sure we can select all of them, and also that list plus the resource type that are not selectable defined in
+a list here are echo to all of the resource types
 
-# we fine end classes from here, but do not include all of the nodes we can select resource type from
+we fine end classes from here, but do not include all of the nodes we can select resource type from
 
-#########
-# select modified: find all node types, define ways to modify each one of them in the happy path project, modify one of them,
-# run select state:modified and make sure the modified node is selected
+---
+select modified: find all node types, define ways to modify each one of them in the happy path project, modify one of them,
+run select state:modified and make sure the modified node is selected
+"""
